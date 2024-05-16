2086a118f6ed50e0036484199684de3d9d14e2a0
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-05-16 15:38:55 +0300

    nav view created

---
+++ src/views/nav.js
@@ -0,0 +1,23 @@
+import { html } from '../lib/lit-html.js';
+import { CONFIG } from '../config.js';
+
+const SPECIAL_USER_ID = CONFIG.SPECIAL_USER_ID;
+
+export const navTemplate = (hasUser, userId) => {
+    return html`
+      <nav>
+            <a href="/">Home</a>
+            <a href="/blogs">Blogs</a>
+            ${hasUser
+        ? html`
+            ${userId === SPECIAL_USER_ID
+        ? html`<a href="/create">Create</a>`
+        : ''}
+            <a href="javascript:void(0)">Logout</a>`
+        : html`
+            <a href="/register">Register</a>
+            <a href="/login">Login</a>
+            `}
+        </nav>
+    `;
+};
