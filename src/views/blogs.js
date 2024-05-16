2f0e2a8b9b11be3e1b6321f1faace31cebe2813e
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-05-16 15:39:59 +0300

    Blog rendering template added

---
+++ src/views/blogs.js
@@ -0,0 +1,32 @@
+import { html } from '../lib/lit-html.js';
+
+import * as blogService from '../data/blog.js';
+import { repeat } from '../lib/directives/repeat.js';
+
+const blogTemplate = (list) => html`
+    <h1>All blogs</h1>
+    ${list}
+`;
+
+const listTemplate = (blogs) => html`
+    <section>
+        ${repeat(blogs, b => b.objectId, blogCard)}
+    </section>
+`;
+
+const blogCard = (blog) => html`
+    <article>
+        <p>${blog.name}</p>
+        <p>${blog.author}</p>
+        <p>${blog.blogCount}</p>
+        <p><a href="/blogs/${blog.objectId}">View Details</a></p>
+    </article>
+`;
+
+export async function blogView(ctx) {
+    ctx.render(blogTemplate(html`<p>Loading...</p>`));
+
+    const { results: blogs } = await blogService.getAll();
+
+    ctx.render(blogTemplate(listTemplate(blogs)));
+};
