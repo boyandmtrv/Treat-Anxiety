f5aa883e65ee0ffdd39f6851e01104d8e928041f
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-05-20 17:26:46 +0300

    comments and reviews funcitons added

--- src/data/blog.js
+++ src/data/blog.js
@@ -6,6 +6,10 @@ const endPoints = {
     'blogs': `/classes/Blog?where=${encodeObject({ readyForRead: true })}&include=owner`,
     'blogsWithUser': (userId) => `/classes/Blog?where=${encodeObject({ $or: [{ readyForRead: true }, filterRelation('owner', '_User', userId)] })}&include=owner`,
     'blogById': `/classes/Blog/`,
+    'commentsByBlogId': (blogId) => `/classes/Comments?where=${encodeObject({ blog: { "__type": "Pointer", "className": "Blog", "objectId": blogId } })}&include=owner`,
+    'comment': `/classes/Comments`,
+    'reviewsByBlogId': (blogId) => `/classes/Reviews?where=${encodeObject({ blog: { "__type": "Pointer", "className": "Blog", "objectId": blogId } })}&include=owner`,
+    'review': `/classes/Reviews`
 };
 
 const SPECIAL_USER_ID = CONFIG.SPECIAL_USER_ID;
@@ -36,4 +40,28 @@ export async function update(id, blogData, userId) {
 
 export async function deleteById(id) {
     return del(endPoints.blogById + id)
+}
+
+export async function getCommentsByBlogId(blogId) {
+    return get(endPoints.commentsByBlogId(blogId));
+}
+
+export async function createComment(commentData) {
+    return post(endPoints.comment, commentData);
+}
+
+export async function deleteComment(commentId) {
+    return del(endPoints.comment + '/' + commentId);
+}
+
+export async function getReviewsByBlogId(blogId) {
+    return get(endPoints.reviewsByBlogId(blogId));
+}
+
+export async function createReview(reviewData) {
+    return post(endPoints.review, reviewData);
+}
+
+export async function deleteReview(reviewId) {
+    return del(endPoints.review + '/' + reviewId);
 }
