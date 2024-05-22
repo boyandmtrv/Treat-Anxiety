ced1fbe5cc0f7144776ad3817a418471003e1d5b
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-05-22 16:30:40 +0300

    login page almost finished

--- src/views/login.js
+++ src/views/login.js
@@ -4,18 +4,8 @@ import { submitHandler } from '../util.js';
 
 const loginTemplate = (onSubmit) => html`
      <div class="row vh-100 g-0">
-        <div class="col-lg-6 position-relative d-none d-lg-block">
-            <div 
-                class="bg-holder"
-                style="background-image:url(src/img/forest.jpg)">
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="row align-items-center justify-content-center h-100 g-0 px-4 px-sm-0">
-                <div class="col col-sm-6 col-lg-7 col-xl-6">
-                    <a href="/" class="d-flex justify-content-center">
-                        <img src="src/img/logo.png" alt="" width="60">
-                    </a>
+            <div class="bg-forms row align-items-center justify-content-center h-100 g-0 px-4 px-sm-0">
+                <div class="col col-sm-6 col-lg-4 col-xl-3">
                     <div class="text-center mb-5">
                         <h3 class="fw-bold">Login</h3>
                         <p class="text-secondary">Get access</p>
@@ -57,7 +47,7 @@ const loginTemplate = (onSubmit) => html`
                             </label>
                         </div>
                         <button
-                            class="btn btn-primary btn-lg w-100 mb-3">
+                            class="btn register-btn btn-lg w-100 mb-3">
                             Login
                         </button>
                     </form>
@@ -69,7 +59,6 @@ const loginTemplate = (onSubmit) => html`
                     </div>
                 </div>
             </div>
-        </div>
     </div>
 `
 
