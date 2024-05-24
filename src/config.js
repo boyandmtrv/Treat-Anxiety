e148d81ead36dcf82c1cf3993158ec1c44eb7c6a
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-05-24 16:59:05 +0300

    minor change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    SPECIAL_USER_ID: 'gTdZq5XKuL',
+    SPECIAL_USER_ID: 'notspecial',
     APP_ID: 'SH6wEc7YmYZA62Z6dtXmHha1b7ru2ZOl0hfoLKAJ',
     API_KEY: '8zXIeGxIY9mjTMTpqvsnTiUiTe5DSZbgVm2jUkwA'
 };
