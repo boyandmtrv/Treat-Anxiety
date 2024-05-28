394c64dec1b2bc2690d6fb8b6e3cbc53283b27d9
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-05-28 12:36:18 +0300

    testing redirects

--- src/views/notFound.js
+++ src/views/notFound.js
@@ -1,13 +1,9 @@
 import { html } from '../lib/lit-html.js';
 
-const notFoundTemplate = () => html`
-      <section id="not-found">
-            <h1>Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
-            <a href="/">Go to Home</a>
-        </section>
+export const notFoundView = () => html`
+    <div class="container text-center mt-5">
+        <h1>Page Not Found</h1>
+        <p>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+        <a class="btn btn-primary" href="/">Back to Home</a>
+    </div>
 `;
-export async function notFoundView(ctx) {
-    ctx.render(notFoundTemplate());
-};
-
