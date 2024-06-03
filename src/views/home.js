acf87106a05fee98983a67ccad83f6314fa1c01f
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-06-03 13:56:11 +0300

    replacing images and first test on the new homepage

--- src/views/home.js
+++ src/views/home.js
@@ -5,11 +5,11 @@ const homeTemplate = () => html`
         <div id="carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
             <div class="carousel-controls">
                 <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" style="background: url(src/img/main-img.png)"></button>
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2" style="background: url(src/img/second-main.png)"></button>
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3" style="background: url(src/img/third-main.png)"></button>
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="3" aria-label="Slide 4" style="background: url(src/img/fourth-main.png)"></button>
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="4" aria-label="Slide 5" style="background: url(src/img/fifth-main.png)"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" style="background: url(src/img/first-main.jpg)"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2" style="background: url(src/img/fifth-main.jpg)"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3" style="background: url(src/img/third-main.jpg)"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="3" aria-label="Slide 4" style="background: url(src/img/fourth-main.jpg)"></button>
+                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="4" aria-label="Slide 5" style="background: url(src/img/second-main.jpg)"></button>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
                     <div>
@@ -26,16 +26,17 @@ const homeTemplate = () => html`
             <div class="carousel-inner">
                 <div 
                     class="carousel-item active"
-                    style="background: url(src/img/main-img.png)">
+                    style="background: url(src/img/first-main.jpg)">
                     <div class="container">
-                        <h2 class="title"><span class="title-treat">Treat</span> Anxiety</h2>
+                    <img src="/src/img/logo.svg" alt="">
+                        <h2 class="title"><span class="title-treat">treat</span> anxiety</h2>
                         <p class="subtitle">you are not your thoughts</p>
                         <a class="nav-link" href="/blogs">See All Blogs</a>
                     </div>
                 </div>
                 <div 
                     class="carousel-item"
-                    style="background: url(src/img/second-main.png)">
+                    style="background: url(src/img/fifth-main.jpg)">
                     <div class="container">
                         <h2>Safe space</h2>
                         <p>dedicated to sharing thoughts and experiences related to anxiety</p>
@@ -43,7 +44,7 @@ const homeTemplate = () => html`
                 </div>
                 <div 
                     class="carousel-item"
-                    style="background: url(src/img/third-main.png)">
+                    style="background: url(src/img/third-main.jpg)">
                     <div class="container">
                         <h2>You are not alone</h2>
                         <p>we aim to remind you that you are not alone. We strive to uncover the hidden peace within each of us, promoting the idea that our thoughts do not define us.</p>
@@ -51,7 +52,7 @@ const homeTemplate = () => html`
                 </div>
                 <div 
                     class="carousel-item"
-                    style="background: url(src/img/fourth-main.png)">
+                    style="background: url(src/img/fourth-main.jpg)">
                     <div class="container container-quote">
                         <p class="quote-text">“Worrying is carrying tomorrow’s load with today’s strength — carrying two days at once. It is moving into tomorrow ahead of time. Worrying doesn’t empty tomorrow of its sorrow, it empties today of its strength.”</p>
                         <h2 class="quote-author">— Corrie ten Boom</h2>
@@ -59,7 +60,7 @@ const homeTemplate = () => html`
                 </div>
                 <div 
                     class="carousel-item"
-                    style="background: url(src/img/fifth-main.png)">
+                    style="background: url(src/img/second-main.jpg)">
                     <div class="container">
                         <h2 class="social">
                             <a href="https://www.instagram.com/treatanxiety/" target="_blank" rel="noopener noreferrer">
