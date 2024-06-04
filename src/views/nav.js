6e1ecd7a02563da4b711bba3ff1dc4ab42df18d7
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-06-04 15:11:40 +0300

    restyled navbar

--- src/views/nav.js
+++ src/views/nav.js
@@ -7,7 +7,9 @@ export const navTemplate = (hasUser, userId) => {
     return html`
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
             <div class="container">
-                <a class="navbar-logo navbar-brand fs-4" href="/">       <img src="/src/img/logo.svg" alt=""></a>
+                <a class="navbar-logo navbar-brand fs-4" href="/">       
+                    <img src="/src/img/logo.svg" alt="">
+                </a>
                 <button 
                     class="navbar-toggler shadow-none border-0" 
                     type="button" 
@@ -21,7 +23,9 @@ export const navTemplate = (hasUser, userId) => {
                 <div class="sidebar offcanvas offcanvas-start" tabindex="-1" 
                      id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header text-white border-bottom">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">\
+                        <a class="instagram-icon-top px-3 py-1 rounded-4 text-decoration-none" href="https://www.instagram.com/treatanxiety/" target="_blank"><i class='bx bxl-instagram'></i></a>
+                        </h5>
                         <button type="button" class="btn-close btn-close-white shadow-none" 
                                 data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
@@ -34,26 +38,28 @@ export const navTemplate = (hasUser, userId) => {
                                 <a class="nav-link" href="/blogs">Blogs</a>
                             </li>
                             ${hasUser
-            ? html`
-                                    ${userId === SPECIAL_USER_ID
-                    ? html`
+                                ? html`
+                                        ${userId === SPECIAL_USER_ID
+                                        ? html`
                                             <li class="nav-item mx-1" data-bs-dismiss="offcanvas">
                                                 <a class="nav-link" href="/create">Create</a>
                                             </li>
                                         `
-                    : ''}
-                                `
-            : ''}
+                                    : ''}
+                                    `
+                                : ''}
                         </ul>
                         <div class="navbar-bottom d-flex flex-row flex-lg-row justify-content-center align-items-center gap-3 text-decoration-none" data-bs-dismiss="offcanvas">
                             ${hasUser
-            ? html`
-                                    <a class="text-decoration-none px-3 py-1 rounded-4" href="/logout">Logout</a>
-                                `
-            : html`
-                                    <a class="text-decoration-none px-3" href="/login">Login</a>
+                                ? html`
+                                    <a class="text-decoration-none px-3 py-1 rounded-4" href="/logout"  style="margin-left: 35px">Logout</a>
+                                 `
+                                : html`
+                                    <a class="text-decoration-none px-3" href="/login" style="margin-left: 50px">Login</a>
                                     <a class="px-3 py-1 rounded-4 text-decoration-none" href="/register">Register</a>
                                 `}
+                                <a class="instagram-icon px-3 py-1 rounded-4 text-decoration-none" href="https://www.instagram.com/treatanxiety/" target="_blank"><i class='bx bxl-instagram'></i></a>
+
                         </div>
                     </div>
                 </div>
