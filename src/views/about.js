32f7bd7ff41e723ae4a1eea565e479c019c8ce63
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-06-10 13:40:29 +0300

    minor change

--- src/views/about.js
+++ src/views/about.js
@@ -27,7 +27,7 @@ const aboutTemplate = () => html`
                             <p>We believe in the power of shared experiences and the strength that comes from understanding and support. By joining our community, you're taking a step towards finding the hidden peace within you and helping others to do the same. Remember, you are not alone, and you are not your thoughts. Thank you for being a part of our journey. Together, we can navigate the challenges of anxiety and discover the tranquility that lies beyond it.</p>
                         </div>
                         <div class="big-bottom">
-                            <p>treat<span class="second-text">anxiety</span><span class="sub-text"> - Finding the hidden peace.</span></p>
+                            <p>treat<span class="second-text">anxiety</span><span class="sub-text"> - You are not your thoughts.</span></p>
                             <a class="instagram-icon-about" href="https://www.instagram.com/treatanxiety/" target="_blank"><i class='bx bxl-instagram'></i></a>
                         </div>
                     </div>
