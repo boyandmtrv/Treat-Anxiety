b71abd39e03c3d403eadebe4e663d92b747ae70b
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-06-11 16:24:14 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import { inject } from '@vercel/analytics';
 import page from './lib/page.mjs';
 import * as api from './data/users.js';
 import { addRender } from './middlewares/render.js';
@@ -18,8 +17,6 @@ import { detailsView } from './views/details.js';
 import { editView } from './views/edit.js';
 import { notFoundView } from './views/notFound.js'; 
 import { aboutView } from './views/about.js';
-
-inject()
 
 page(addRender(document.querySelector('main'), document.querySelector('header')));
 page(addSession(getUserData));
